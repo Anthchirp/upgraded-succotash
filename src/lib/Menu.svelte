61edb3d700e9cb9cafcb76cc687dcf3c972ca6fb
Markus Gerstel <markus.gerstel@osirium.com>
2025-01-03 19:34:15 +0000

    Menu animation, set up prettier

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,32 +1,36 @@
 <script lang="ts">
-  import { onMount, onDestroy } from "svelte";
-  let menuButton: HTMLButtonElement | null = null;
-  let menuSection: HTMLElement | null = null;
+    import { onMount, onDestroy } from "svelte"
+    let menuButton: HTMLButtonElement | null = null
+    let menuSection: HTMLElement | null = null
 
-  const toggleMenu = () => {
-    menuSection?.classList.toggle("hidden");
-    menuSection?.classList.toggle("flex");
-  };
+    const toggleMenu = () => {
+        menuSection?.classList.toggle("hidden")
+        menuSection?.classList.toggle("flex")
+        menuButton?.classList.toggle("toggle-btn")
+    }
 </script>
 
 <button
-  bind:this={menuButton}
-  onclick={toggleMenu}
-  class="cursor-pointer text-8xl text-sky-600"
+    bind:this={menuButton}
+    onclick={toggleMenu}
+    class="relative w-8 h-8 cursor-pointer text-8xl text-sky-600"
 >
-  &#9776;
+    <div
+        class="bg-white w-8 h-1 rounded absolute top-4 -mt-0.5 before:content-[''] transition-all duration-500 before:bg-white before:h-1 before:rounded before:absolute before:w-8 before:transition-all before:duration-500 before:-translate-x-4 before:-translate-y-3 after:content-[''] after:bg-white after:h-1 after:rounded after:absolute after:w-8 after:transition-all after:duration-500 after:-translate-x-4 after:translate-y-3"
+    ></div>
+    <!-- &#9776; -->
 </button>
 <section
-  bind:this={menuSection}
-  onclick={toggleMenu}
-  class="origin-top animate-open-menu hidden absolute top-0 bg-black w-full flex-col justify-content-center border border-green-400"
+    bind:this={menuSection}
+    onclick={toggleMenu}
+    class="origin-top animate-open-menu hidden absolute top-68 bg-black w-full flex-col justify-content-center border border-green-400"
 >
-  <button class="self-end px-6 text-8xl text-sky-600">&times;</button>
-  <nav class="flex flex-col items-center py-8" aria-label="mobile">
-    <a
-      href="#home"
-      class="w-full text-center py-6 hover:opacity-90 border border-green-300"
-      >Home</a
-    >
-  </nav>
+    <!-- <button class="self-end px-6 text-8xl text-sky-600">&times;</button> -->
+    <nav class="flex flex-col items-center py-8" aria-label="mobile">
+        <a
+            href="#home"
+            class="w-full text-center py-6 hover:opacity-90 border border-green-300"
+            >Home</a
+        >
+    </nav>
 </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,22 @@
-const { transform } = require('typescript');
+const { transform } = require("typescript")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js,svelte,ts}",
-  ],
-  darkMode: 'selector',
-  plugins: [],
-  theme: {
-    extend: {
-      keyframes: {
-        'open-menu': {
-          '0%': { transform: 'scaleY(0)' },
-          '80%': { transform: 'scaleY(1.2)' },
-          '100%': { transform: 'scaleY(1)' },
-        }
-      },
-      animation: {
-        'open-menu': 'open-menu 0.5s ease-in-out forwards',
-      }
+    content: ["./index.html", "./src/**/*.{html,js,svelte,ts}"],
+    darkMode: "selector",
+    plugins: [],
+    theme: {
+        extend: {
+            keyframes: {
+                "open-menu": {
+                    "0%": { transform: "scaleY(0)" },
+                    "80%": { transform: "scaleY(1.2)" },
+                    "100%": { transform: "scaleY(1)" },
+                },
+            },
+            animation: {
+                "open-menu": "open-menu 0.5s ease-in-out forwards",
+            },
+        },
     },
-  },
 }
