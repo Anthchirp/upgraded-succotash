009183073bb39f24db01aa9936ada3cfddf9d56a
Markus Gerstel <markus.gerstel@osirium.com>
2025-01-04 08:21:02 +0000

    New dark mode switch

--- src/lib/ThemeSwitch.svelte
+++ src/lib/ThemeSwitch.svelte
@@ -1,140 +1,112 @@
 <script lang="ts">
     export const browser = !import.meta.env.SSR
-    let darkMode = true
+    let darkModeEnabled = true
+    let buttonLabel = ""
+
+    function updatePageDarkMode() {
+        if (darkModeEnabled) {
+            document.documentElement.classList.add("dark")
+            buttonLabel = "Disable dark mode"
+        } else {
+            document.documentElement.classList.remove("dark")
+            buttonLabel = "Enable dark mode"
+        }
+    }
 
     function handleSwitchDarkMode() {
-        darkMode = !darkMode
-
-        localStorage.setItem("theme", darkMode ? "dark" : "light")
-
-        darkMode
-            ? document.documentElement.classList.add("dark")
-            : document.documentElement.classList.remove("dark")
+        darkModeEnabled = !darkModeEnabled
+        localStorage.setItem("theme", darkModeEnabled ? "dark" : "light")
+        updatePageDarkMode()
     }
 
     if (browser) {
-        if (
+        darkModeEnabled =
             localStorage.theme === "dark" ||
             (!("theme" in localStorage) &&
                 window.matchMedia("(prefers-color-scheme: dark)").matches)
-        ) {
-            document.documentElement.classList.add("dark")
-            darkMode = true
-        } else {
-            document.documentElement.classList.remove("dark")
-            darkMode = false
-        }
+        updatePageDarkMode()
     }
 </script>
 
 <div>
-    <input
-        checked={darkMode}
+    <button
         on:click={handleSwitchDarkMode}
-        type="checkbox"
-        id="theme-toggle"
-    />
-    <label for="theme-toggle" />
-    <!--
-  <button
-    on:click={handleSwitchDarkMode}
-    class="relative inline-flex items-center py-1.5 px-2 rounded-full transition-colors duration-300 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-white focus:outline-none bg-cyan-500 text-cyan-200 focus-visible:ring-cyan-600"
-    id="headlessui-switch-:R1cjpb6:"
-    role="switch"
-    type="button"
-    tabindex="0"
-    aria-checked="false"
-    data-headlessui-state=""
-    ><span class="sr-only">Disable dark mode</span><svg
-      width="24"
-      height="24"
-      fill="none"
-      aria-hidden="true"
-      class="transform transition-transform scale-0 duration-500"
-      ><path
-        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-        fill="currentColor"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></path><path
-        d="M12 4v1M18 6l-1 1M20 12h-1M18 18l-1-1M12 19v1M7 17l-1 1M5 12H4M7 7 6 6"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></path></svg
-    ><svg
-      width="24"
-      height="24"
-      fill="none"
-      aria-hidden="true"
-      class="ml-3.5 transform transition-transform scale-100 duration-300"
-      ><path
-        d="M18 15.63c-.977.52-1.945.481-3.13.481A6.981 6.981 0 0 1 7.89 9.13c0-1.185-.04-2.153.481-3.13C6.166 7.174 5 9.347 5 12.018A6.981 6.981 0 0 0 11.982 19c2.67 0 4.844-1.166 6.018-3.37ZM16 5c0 2.08-.96 4-3 4 2.04 0 3 .92 3 3 0-2.08.96-3 3-3-2.04 0-3-1.92-3-4Z"
-        fill="currentColor"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></path></svg
-    ><span
-      class="absolute top-0.5 left-0.5 bg-white w-8 h-8 rounded-full flex items-center justify-center transition duration-500 transform"
-      ><svg
-        width="24"
-        height="24"
-        fill="none"
-        aria-hidden="true"
-        class="flex-none transition duration-500 transform text-cyan-500 opacity-100 scale-100"
-        ><path
-          d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          fill="currentColor"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path><path
-          d="M12 4v1M18 6l-1 1M20 12h-1M18 18l-1-1M12 19v1M7 17l-1 1M5 12H4M7 7 6 6"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path></svg
-      ><svg
-        width="24"
-        height="24"
-        fill="none"
-        aria-hidden="true"
-        class="flex-none -ml-6 transition duration-500 transform text-slate-700 opacity-0 scale-0"
-        ><path
-          d="M18 15.63c-.977.52-1.945.481-3.13.481A6.981 6.981 0 0 1 7.89 9.13c0-1.185-.04-2.153.481-3.13C6.166 7.174 5 9.347 5 12.018A6.981 6.981 0 0 0 11.982 19c2.67 0 4.844-1.166 6.018-3.37ZM16 5c0 2.08-.96 4-3 4 2.04 0 3 .92 3 3 0-2.08.96-3 3-3-2.04 0-3-1.92-3-4Z"
-          fill="currentColor"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path></svg
-      ></span
-    ></button
-  > -->
+        class="relative inline-flex items-center rounded-full bg-cyan-500 px-2 py-1.5 text-cyan-200 transition-colors duration-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-cyan-600 focus-visible:ring-offset-2 focus-visible:ring-offset-white dark:bg-slate-700 dark:text-slate-400 dark:focus-visible:ring-slate-500"
+        role="switch"
+        type="button"
+        tabindex="0"
+        aria-checked={darkModeEnabled}
+        ><span class="sr-only">{buttonLabel}</span><svg
+            width="24"
+            height="24"
+            fill="none"
+            aria-hidden="true"
+            class="scale-0 transform transition-transform duration-500 dark:scale-100 dark:duration-300"
+            ><path
+                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                fill="currentColor"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            ></path><path
+                d="M12 4v1M18 6l-1 1M20 12h-1M18 18l-1-1M12 19v1M7 17l-1 1M5 12H4M7 7 6 6"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            ></path></svg
+        ><svg
+            width="24"
+            height="24"
+            fill="none"
+            aria-hidden="true"
+            class="duration:300 ml-3.5 scale-100 transform transition-transform dark:scale-0 dark:duration-500"
+            ><path
+                d="M18 15.63c-.977.52-1.945.481-3.13.481A6.981 6.981 0 0 1 7.89 9.13c0-1.185-.04-2.153.481-3.13C6.166 7.174 5 9.347 5 12.018A6.981 6.981 0 0 0 11.982 19c2.67 0 4.844-1.166 6.018-3.37ZM16 5c0 2.08-.96 4-3 4 2.04 0 3 .92 3 3 0-2.08.96-3 3-3-2.04 0-3-1.92-3-4Z"
+                fill="currentColor"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            ></path></svg
+        ><span
+            class="absolute left-0.5 top-0.5 flex h-8 w-8 transform items-center justify-center rounded-full bg-yellow-400 transition duration-500 dark:translate-x-[2.625rem] dark:bg-white"
+            ><svg
+                width="24"
+                height="24"
+                fill="none"
+                aria-hidden="true"
+                class="flex-none scale-100 transform text-black opacity-100 transition duration-500 dark:scale-0 dark:opacity-0"
+                ><path
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    fill="currentColor"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                ></path><path
+                    d="M12 4v1M18 6l-1 1M20 12h-1M18 18l-1-1M12 19v1M7 17l-1 1M5 12H4M7 7 6 6"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                ></path></svg
+            ><svg
+                width="24"
+                height="24"
+                fill="none"
+                aria-hidden="true"
+                class="-ml-6 flex-none scale-0 transform text-slate-700 opacity-0 transition duration-500 dark:scale-100 dark:opacity-100"
+                ><path
+                    d="M18 15.63c-.977.52-1.945.481-3.13.481A6.981 6.981 0 0 1 7.89 9.13c0-1.185-.04-2.153.481-3.13C6.166 7.174 5 9.347 5 12.018A6.981 6.981 0 0 0 11.982 19c2.67 0 4.844-1.166 6.018-3.37ZM16 5c0 2.08-.96 4-3 4 2.04 0 3 .92 3 3 0-2.08.96-3 3-3-2.04 0-3-1.92-3-4Z"
+                    fill="currentColor"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                ></path></svg
+            ></span
+        ></button
+    >
 </div>
-
-<style lang="postcss">
-    #theme-toggle {
-        @apply invisible;
-    }
-
-    #theme-toggle + label {
-        @apply absolute right-24 top-6 inline-block h-12 w-12 cursor-pointer rounded-full duration-300 content-[''];
-    }
-
-    #theme-toggle:not(:checked) + label {
-        @apply bg-amber-400;
-    }
-
-    #theme-toggle:checked + label {
-        @apply bg-transparent;
-        box-shadow: inset -18px -16px 1px 1px #ddd;
-    }
-</style>
